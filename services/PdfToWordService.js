4fd7010f48f02fceae2066158a68b05440eb449e
pptqalitqon20 <pptqalitqon767@gmail.com>
2025-12-31 23:57:47 +0800

    Create PdfToWordService.js

---
+++ services/PdfToWordService.js
@@ -0,0 +1,20 @@
+const { exec } = require('child_process');
+const path = require('path');
+
+function convertPdfToWord(pdfPath, docxPath) {
+  return new Promise((resolve, reject) => {
+    const scriptPath = path.join(__dirname, '../tools/pdf_to_word.py');
+
+    const cmd = `python "${scriptPath}" "${pdfPath}" "${docxPath}"`;
+
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        console.error('Python stderr:', stderr);
+        return reject(err);
+      }
+      resolve(docxPath);
+    });
+  });
+}
+
+module.exports = { convertPdfToWord };
